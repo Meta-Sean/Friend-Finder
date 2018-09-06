7fa38d7f427690f34c42fd7ab9cc965855728b29
Sean Huckabee <seanhuckabee@gmail.com>
2018-09-06 17:11:09 -0500

    heroku changes

--- app/data/connection.js
+++ app/data/connection.js
@@ -1,14 +1,21 @@
 var mysql = require("mysql");
-// MySQL DB Connection Information (remember to change this with our specific credentials)
-var connection = mysql.createConnection({
+// MySQL DB Connection Information 
+var connection;
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "root",
     database: "friend_finder_db"
   });
+
+}
+
   
-
   //EXPORT CONNECTION FOR EXPRESS SERVER.JS to use
-
+  connection.connect();
   module.exports = connection;
